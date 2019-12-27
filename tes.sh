35836f0d9ea4335d29209e6deadf8d873928e014
WsHack648 <59272735+WsHack648@users.noreply.github.com>
2019-12-27 12:40:32 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,51 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By Ws Hack
+echo $green"Selamat Datang"
+echo
+sleep 2
+echo $yellow"silahkan pilih no nya :"
+echo
+echo $cyan"1.) Stabilkan Jaringan"
+echo
+echo $purple"2.) Menambahkan Kecepatan Jaringan"
+echo
+echo $red"0.) keluar"
+echo
+echo $white
+read -p "pilih mana: " bro
+if [ $bro = 1 ] || [ $bro = 1]
+then
+clear
+toilet -f big -F gay Hack 11 Back
+echo $green"Stabilkan Jaringan by WS HACK"
+sleep1
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo toilet -f big -F gay Hack 11 Back
+echo $cyan"Menambahkan Keceatan Jaringan"
+sleep1
+ping -s1000 8.8.8.8
+fi
+ 
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "Bye"
+echo $white"Sampai Jumpa"
+sleep 1
+exit
+fi
